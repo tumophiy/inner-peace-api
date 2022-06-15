52779b19bbe665ad50d492614f75749fc93df362
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-06-15 09:23:41 +0300

    first commit

---
+++ db/migrate/20220615061955_create_goals.rb
@@ -0,0 +1,11 @@
+class CreateGoals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :goals do |t|
+      t.string :title
+      t.text :description
+      t.date :deadline
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/goals.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :goal do
+    title { "MyString" }
+    description { "MyText" }
+    deadline { "2022-06-15" }
+  end
+end
