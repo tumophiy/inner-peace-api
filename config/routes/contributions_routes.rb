f384acf6f13cec2dd0d6d2a7b9574970474061b9
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-11 18:28:44 +0300

    change routes from contribution to contributions

--- config/routes/contributions_routes.rb
+++ config/routes/contributions_routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   scope 'api' do
     scope '/goals/:goals_id' do
-      resources :contribution, only: %i[create show index update destroy]
+      resources :contributions, only: %i[create show index update destroy]
     end
   end
 end
