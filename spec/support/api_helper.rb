5ba4d32d83b91106b5b024e78c47c2ecbd6f9a42
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-15 15:39:37 +0300

    remove json_data method from ApiHelpers

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 module ApiHelpers
-  def json
-    JSON.parse(response.body).deep_symbolize_keys
-  end
-
-  def json_data
-    json[:data]
+  def response_body
+    JSON.parse(response.body).deep_symbolize_keys[:data]
   end
 end
