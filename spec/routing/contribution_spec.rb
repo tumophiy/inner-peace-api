1dd2aa51c1f687fd8fbebb712f9810c0fb4cf3e2
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-15 16:30:25 +0300

    delete tests for routes

--- spec/routing/contribution_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe '/contributions routes' do
-  it 'routes to contributions#index' do
-    expect(get('/contributions')).to route_to('contributions#index')
-  end
-end
--- spec/routing/goal_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe '/goals routes' do
-  it 'routes to goals#show' do
-    expect(get('/goals/1')).to route_to('goals#show', id: '1')
-  end
-end
