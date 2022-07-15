6f72915e57072383f868d20ebde5a41436aaa7b7
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-15 11:32:49 +0300

    change factories in order to use faker gem

--- spec/factories/contributions.rb
+++ spec/factories/contributions.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :contribution do
-    amount { 1 }
-    description { 'MyString' }
-    expected_date { '2022-06-22' }
+    amount { Faker::Number.number(digits: 3) }
+    description { Faker::Lorem.sentence }
+    expected_date { Faker::Date.forward(days: 23) }
     association :goal_id
   end
 end
--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :goal do
-    amount { 1000 }
-    interest_rate { 10 }
-    title { 'MyString' }
-    description { 'MyText' }
-    deadline { '2022-06-15' }
+    amount { Faker::Number.number(digits: 6) }
+    interest_rate { Faker::Number.between(from: 0.0, to: 100) }
+    title { Faker::Lorem.word }
+    description { Faker::Lorem.sentence }
+    deadline { Faker::Date.between(from: 1.years.since, to: 10.years.since) }
   end
 end
