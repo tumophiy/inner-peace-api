b0940203947ce07cdbfbd5d38a2ed635fb6ef840
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-15 13:12:18 +0300

    add recent scope, validates numericality of amount and tests for this

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,8 +3,10 @@
 class Goal < ApplicationRecord
   has_many :contributions
   validates :title, presence: true
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
   validates :deadline, presence: true
   validates :interest_rate, presence: true
+
+  scope :recent, -> { order(created_at: :desc) }
 end
