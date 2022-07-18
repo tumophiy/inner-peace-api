45e3cf8f71068abb3baf57c73fcec013e5834701
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-18 12:05:35 +0300

    devise/jwt settup, add tables users and jwt_denylist

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,29 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_622_105_825) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_090314) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'contributions', force: :cascade do |t|
-    t.integer 'amount', null: false
-    t.string 'description', null: false
-    t.date 'expected_date'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'goal_id', null: false
-    t.index ['goal_id'], name: 'index_contributions_on_goal_id'
+  create_table "contributions", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.string "description", null: false
+    t.date "expected_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "goal_id", null: false
+    t.index ["goal_id"], name: "index_contributions_on_goal_id"
   end
 
-  create_table 'goals', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.date 'deadline'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'amount'
-    t.integer 'interest_rate'
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "deadline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "amount"
+    t.integer "interest_rate"
   end
 
-  add_foreign_key 'contributions', 'goals'
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "contributions", "goals"
 end
