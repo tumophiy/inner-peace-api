cfb0a280f4f154746ba01b82ca73a33eea001e44
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-18 12:42:07 +0300

    add routes for overwritten devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   def load_routes_folder(base_foldername)
     this_directory = File.dirname(File.expand_path(__FILE__))
     route_folder_path = File.expand_path(base_foldername, this_directory)
