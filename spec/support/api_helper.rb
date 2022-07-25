f414d284f8bdc30e363f61c5c64719d25e9ec9bc
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-25 17:13:32 +0300

    fix files in order to pass tests

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -4,4 +4,18 @@ module ApiHelpers
   def response_body
     JSON.parse(response.body).deep_symbolize_keys[:data]
   end
+
+  def confirm_and_login_user(user)
+    get '/users/confirm', params: { token: user.confirmation_token }
+    post '/users/login', params: { email: user.email, password: 'password' }
+    json['auth_token']
+  end
+
+  def login_user
+    before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user = FactoryBot.create(:user)
+      sign_in_user
+    end
+  end
 end
