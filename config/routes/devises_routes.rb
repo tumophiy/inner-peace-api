0bad378fc702b28dae71431de8bbf20637bfb2a0
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-25 17:12:04 +0300

    add /api scope to devises_routes

--- config/routes/devises_routes.rb
+++ config/routes/devises_routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
+  scope 'api' do
+    devise_for :users,
+               controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+               }
+  end
 end
