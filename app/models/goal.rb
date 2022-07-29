e0c85e8936a3e12b2edcaaeb9f3be667b34c2403
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-07-29 18:02:26 +0300

    feat(models/goal.rb): add enum for status

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -9,4 +9,11 @@ class Goal < ApplicationRecord
   validates :interest_rate, presence: true
 
   scope :recent, -> { order(created_at: :desc) }
+
+  enum status: {
+    initialized: 0,
+    in_progress: 10,
+    closed: 20,
+    deleted: 30
+  }
 end
